909fe98f72f9259ad924db9887a3012abda0891e
NehemiahAklil <nehemiahaklil@gmail.com>
2026-01-19 02:54:02 +0300

    chore: added markdown formatting

--- lua/neotredis/plugins/formatting.lua
+++ lua/neotredis/plugins/formatting.lua
@@ -19,6 +19,7 @@ return {
 				rust = { "rustfmt", lsp_format = "fallback" },
 				-- Conform will run the first available formatter
 				javascript = { "prettierd", "prettier", stop_after_first = true },
+				markdown = { "prettierd", "prettier", stop_after_first = true },
 				typescript = { "prettierd", "prettier", stop_after_first = true },
 				go = { "gofmt" },
 				--
