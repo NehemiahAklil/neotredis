63da3fbad393effe66575c6777bba28d96970d7c
NehemiahAklil <nehemiahaklil@gmail.com>
2026-01-24 16:35:29 +0300

    chore: made dashboard sexy

--- lua/neotredis/plugins/dashboard.lua
+++ lua/neotredis/plugins/dashboard.lua
@@ -25,9 +25,8 @@ return {
             ██║ ╚████║███████╗╚██████╔╝   ██║   ██║  ██║███████╗██████╔╝██║███████║
             ╚═╝  ╚═══╝╚══════╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝╚══════╝╚═════╝ ╚═╝╚══════╝
         ]]
-
 		logo = string.rep("\n", 8) .. logo .. "\n\n"
-		require("dashboard").setup({
+		opts = {
 			theme = "doom",
 			config = {
 				header = vim.split(logo, "\n"),
@@ -36,33 +35,48 @@ return {
 				},
 				center = {
 					{
-						icon = " ",
-						icon = " ",
-						desc = "Find File",
-						key = "f",
-						keymap = "Spc f",
-						action = "Telescope find_files",
-					},
-					{
 						icon = " ",
-						desc = "Recent Projects",
-						key = "p",
-						keymap = "Spc f",
-						action = "Telescope projects",
-					},
-					{
-						icon = " ",
+						icon_hl = "Title",
 						desc = "Recent File",
+						desc_hl = "Number",
 						key = "o",
 						keymap = "Spc f",
+						key_hl = "Number",
+						key_format = " %s", -- remove default surrounding `[]`
 						action = "Telescope oldfiles",
 					},
 					{
-						icon = "  ",
+						icon = " ",
+						icon_hl = "Title",
+						desc = "Recent Projects          ",
+						desc_hl = "Number",
+						key = "p",
+						keymap = "Spc f",
+						key_hl = "Number",
+						key_format = " %s", -- remove default surrounding `[]`
+						action = "Telescope projects",
+					},
+					{
+						icon = " ",
+						icon_hl = "Title",
 						desc = "Open Folder",
+						desc_hl = "String",
 						key = "O",
 						keymap = "Spc f",
+						key_hl = "String",
+						key_format = " %s", -- remove default surrounding `[]`
 						action = "Telescope file_browser",
+					},
+					{
+						icon = " ",
+						icon_hl = "Title",
+						desc = "Find File",
+						desc_hl = "String",
+						key = "f",
+						keymap = "SPC f",
+						key_hl = "String",
+						key_format = " %s", -- remove default surrounding `[]`
+						action = "Telescope find_files",
 					},
 					-- {
 					-- 	icon = "  ",
@@ -74,6 +88,12 @@ return {
 				},
 				footer = {},
 			},
-		})
+		}
+
+		-- for _, button in ipairs(opts.config.center) do
+		-- 	button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+		-- 	button.key_format = "  %s"
+		-- end
+		require("dashboard").setup(opts)
 	end,
 }
