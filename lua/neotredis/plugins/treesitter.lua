deba3124611f693116fe750d3aba410824f486a0
NehemiahAklil <nehemiahaklil@gmail.com>
2026-03-27 12:16:43 +0300

    chore: clean up code from config variable to opts on treesitter

--- lua/neotredis/plugins/treesitter.lua
+++ lua/neotredis/plugins/treesitter.lua
@@ -1,51 +1,45 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	cmd = "TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			-- A list of parser names, or "all" (the listed parsers MUST always be installed)
-			ensure_installed = {
-				"bash",
-				"c",
-				"cpp",
-				"css",
-				"dart",
-				"diff",
-				"gitignore",
-				"go",
-				"gomod",
-				"gosum",
-				"graphql",
-				"html",
-				"javascript",
-				"json",
-				"lua",
-				"lua",
-				"markdown",
-				"markdown_inline",
-				"typescript",
-				"vim",
-				"vue",
-				"xml",
-				"yaml",
-			},
-
-			-- Install parsers synchronously (only applied to `ensure_installed`)
-			sync_install = false,
-
-			-- Automatically install missing parsers when entering buffer
-			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-			auto_install = true,
-
-			highlight = {
-				enable = true,
-
-				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-				-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-				-- Using this option may slow down your editor, and you may see some duplicate highlights.
-				-- Instead of true it can also be a list of languages
-				additional_vim_regex_highlighting = false,
-			},
-		})
-	end,
+	opts = {
+		auto_install = true,
+		indent = { enable = true }, ---@type lazyvim.TSFeat
+		highlight = { enable = true, additional_vim_regex_highlighting = false }, ---@type lazyvim.TSFeat
+		folds = { enable = true }, ---@type lazyvim.TSFeat
+		-- A list of parser names, or "all" (the listed parsers MUST always be installed)
+		ensure_installed = {
+			"bash",
+			"c",
+			"css",
+			"dart",
+			"diff",
+			"gitignore",
+			"go",
+			"gomod",
+			"gosum",
+			"graphql",
+			"html",
+			"javascript",
+			"jsdoc",
+			"json",
+			"jsonc",
+			"lua",
+			"luadoc",
+			"luap",
+			"markdown",
+			"markdown_inline",
+			"printf",
+			"python",
+			"query",
+			"regex",
+			"toml",
+			"tsx",
+			"typescript",
+			"vim",
+			"vimdoc",
+			"vue",
+			"xml",
+			"yaml",
+		},
+	},
 }
